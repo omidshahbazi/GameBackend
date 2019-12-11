3c39377cfdde167738177c271532f8faa6f787ed
Omid Shahbazi <sh.omid.m@gmail.com>
2019-12-11 14:44:40 +0330

    Modified   Binaries/Server/Configs/Server.json Modified   Project/Core.NetCore/Core.NetCore.csproj Modified   Project/Core.NetFramework/Core.NetFramework.csproj Modified   Source/Core/Application.cs

--- Source/Core/Application.cs
+++ Source/Core/Application.cs
@@ -1,6 +1,8 @@
 // Copyright 2019. All Rights Reserved.
+using Backend.Base.Configs;
 using GameFramework.Common.FileLayer;
 using GameFramework.Common.MemoryManagement;
+using GameFramework.Networking;
 using System;
 
 namespace Backend.Core
@@ -16,6 +18,21 @@ namespace Backend.Core
 			FileSystem.DataPath = Environment.CurrentDirectory + "/../";
 
 			Configs.Instance.Initialize();
+
+			Server serverInfo = Configs.Instance.Server;
+			for (int i = 0; i < serverInfo.Sockets.Length; ++i)
+			{
+				Server.Socket socketInfo = serverInfo.Sockets[i];
+
+				for (int j = 0; j < socketInfo.Ports.Length; ++j)
+				{
+					TCPServerSocket socket = new TCPServerSocket();
+
+					socket.Bind(socketInfo.Host, socketInfo.Ports[j]);
+
+					socket.Listen();
+				}
+			}
 		}
 	}
 }
