c767c8dabbb4787dd2923abc2cd32fba118e509b
Omid Shahbazi <sh.omid.m@gmail.com>
2019-12-24 22:49:52 +0330

    Modified   Project/NetFramework/Base/Base.csproj Modified   Source/Core/NetworkSystem/RequestManager.cs

--- Source/Core/NetworkSystem/RequestManager.cs
+++ Source/Core/NetworkSystem/RequestManager.cs
@@ -1,5 +1,4 @@
 // Copyright 2019. All Rights Reserved.
-using Backend.Base.Utilities;
 using Backend.Core.LogSystem;
 using Backend.Core.NetworkSystem;
 using GameFramework.Common.MemoryManagement;
@@ -10,6 +9,51 @@ namespace Backend.Core
 {
 	class RequestManager : Singleton<RequestManager>
 	{
+		//private static TypeMap types;
+
+		//public static void RegisterType<T>()
+		//{
+		//	RegisterType(typeof(T));
+		//}
+
+		//public static bool RegisterType(Type Type)
+		//{
+		//	if (Type == null)
+		//		return false;
+
+		//	uint hash = Type.MakeHash();
+
+		//	if (types.ContainsKey(hash))
+		//		return true;
+
+		//	types[hash] = Type;
+
+		//	MemberInfo[] members = Type.GetMemberVariables(ReflectionExtensions.AllNonStaticFlags);
+		//	for (int i = 0; i < members.Length; ++i)
+		//	{
+		//		MemberInfo member = members[i];
+		//		Type type = null;
+
+		//		if (member is FieldInfo)
+		//			type = ((FieldInfo)member).FieldType;
+		//		else if (member is PropertyInfo)
+		//			type = ((PropertyInfo)member).PropertyType;
+
+		//		if (type.IsPrimitive || type.IsEnum || type == typeof(string))
+		//			continue;
+
+		//		if (type.IsArray)
+		//			type = type.GetElementType();
+
+		//		if (!RegisterType(type))
+		//			return false;
+		//	}
+
+		//	return true;
+		//}
+
+
+
 		private class RequestMap : Dictionary<uint, Action<Client, object>>
 		{ }
 
