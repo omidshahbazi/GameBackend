90cf0a956471cd76eeb126b3f87810767e4aefd4
Omid Shahbazi <sh.omid.m@gmail.com>
2020-01-09 00:34:01 +0330

    Modified   Project/Client/Client/Client.csproj Modified   Project/NetCore/Base/Base.NetCore.csproj Modified   Project/NetCore/Common/Common.NetCore.csproj Modified   Project/NetCore/Core/Core.NetCore.csproj Modified   Project/NetCore/Database/Database.NetCore.csproj Modified   Project/NetCore/Metric/Metric.NetCore.csproj Modified   Project/NetCore/Standalone/Standalone.NetCore.csproj Modified   Project/NetFramework/Common/Common.NetFramework.csproj Modified   Project/NetFramework/Core/Core.NetFramework.csproj Modified   Project/NetFramework/Database/Database.NetFramework.csproj Modified   Project/NetFramework/Metric/Metric.NetFramework.csproj Modified   Project/Tests/ClientTest/ClientTest.csproj Modified   Project/Tests/ClientTest/Program.cs Modified   Project/Tests/ServerTest/ServerTest.csproj Modified   Source/Metric/MetricCollector.cs

--- Project/Tests/ClientTest/Program.cs
+++ Project/Tests/ClientTest/Program.cs
@@ -20,7 +20,7 @@ namespace ClientTest
 			connection.OnConnectionFailed += Connection_OnConnectionFailed;
 			connection.OnDisconnected += Connection_OnDisconnected;
 
-			connection.Connect(ProtocolTypes.UDP, "::1", 5000);
+			connection.Connect(ProtocolTypes.TCP, "::1", 5000);
 
 			while (true)
 			{
